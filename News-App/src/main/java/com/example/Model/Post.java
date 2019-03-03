f3aa1c7ae208a9acc3753c860cf534fe5690f356
lamya <lamyyamessaoudi@gmail.com>
2019-03-03 17:34:22 +0100

    ajout en post

--- News-App/src/main/java/com/example/Model/Post.java
+++ News-App/src/main/java/com/example/Model/Post.java
@@ -2,14 +2,12 @@ package com.example.Model;
 
 import java.util.List;
 
+import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import org.springframework.data.annotation.Id;
-
 public class Post {
-
 	@Id
 	private Long idPost;
 	private String TitlePost;
@@ -24,8 +22,6 @@ public class Post {
 	private Admin admin;
 	@OneToMany
 	private List<Like> listLike;
+	@OneToMany
 	private List<Comment> listComment;
-	
-	
-	
 }
